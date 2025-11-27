bd6e537aacbba39513b6a2e6532f6ab7621c9f12
silas.martens <silas.martens@stud.tu-darmstadt.de>
2025-11-27 13:35:09 +0100

    reformat

--- tools/cgdiff/CGDiff.cpp
+++ tools/cgdiff/CGDiff.cpp
@@ -20,8 +20,8 @@ std::vector<std::unique_ptr<metacg::Diff>> compare(const metacg::Callgraph& mcgA
   using Set = std::unordered_set<NodeSummary, NodeSummaryHasher, NodeSummaryComparator>;
 
   ComparisonMode nameOnlyMode = ComparisonMode::ignoreBody | ComparisonMode::ignoreEdges |
-    ComparisonMode::ignoreMetadata | ComparisonMode::ignoreEdgeMetadata |
-    ComparisonMode::ignoreGlobalMetadata;
+                                ComparisonMode::ignoreMetadata | ComparisonMode::ignoreEdgeMetadata |
+                                ComparisonMode::ignoreGlobalMetadata;
 
   NodeSummaryHasher nameHasher{nameOnlyMode};
   NodeSummaryComparator nameComparator{nameOnlyMode};
@@ -159,7 +159,7 @@ int main(int argc, char** argv) {
                              "Returns 0 if call-graphs are equal, 1 otherwise.\n");
 
     options.add_options()("ignore-edges", "ignore edges", cxxopts::value<bool>()->default_value("false"))(
-      "ignore-hasBody", "ignore hasBody", cxxopts::value<bool>()->default_value("false"))(
+        "ignore-hasBody", "ignore hasBody", cxxopts::value<bool>()->default_value("false"))(
         "ignore-md", "ignore node metadata", cxxopts::value<bool>()->default_value("false"))(
         "ignore-edge-md", "ignore edge metadata", cxxopts::value<bool>()->default_value("false"))(
         "ignore-global-md", "ignore global metadata", cxxopts::value<bool>()->default_value("false"))(
@@ -246,7 +246,7 @@ int main(int argc, char** argv) {
 
       *out << DiffFormatter::emitAsJson(diffs, ignoring, std::filesystem::absolute(cg1).string(),
                                         std::filesystem::absolute(cg2).string())
-        .dump();
+                  .dump();
     }
 
     if (diffs.empty()) {
