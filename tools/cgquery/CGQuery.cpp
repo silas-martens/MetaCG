e4a40e43c2f09f8a8e64041a29be8bbaa6f4d484
silas.martens <silas.martens@stud.tu-darmstadt.de>
2025-12-18 13:49:47 +0100

    Add CGQuery CLI

--- tools/cgquery/CGQuery.cpp
+++ tools/cgquery/CGQuery.cpp
@@ -1,98 +1,122 @@
+#include "Callgraph.h"
+#include "CgNode.h"
+#include "CgTypes.h"
 #include "ReachabilityAnalysis.h"
+#include "DominatorAnalysis.h"
 #include "io/MCGReader.h"
 #include <cxxopts.hpp>
 #include <iostream>
 
+
 int main(int argc, char** argv) {
-    if (argc < 2) {
+    if (argc < 3) {
         std::cerr << "Usage: cgquery <command> [options] <input_file>\n";
         return 1;
     }
 
     std::string command = argv[1];
-    std::vector<char*> args(argv + 2, argv + argc);
-
     std::string cg_name = argv[argc-1];
 
-    if (command == "reaches") {
-        cxxopts::Options options("cgquery reachability", "Check function reachability");
+    auto fs = metacg::io::FileSource(cg_name);
+    std::unique_ptr<metacg::io::MCGReader> r1 = metacg::io::createReader(fs);
+    auto cg = r1->read();
+
+    // Build argv vector including program name for cxxopts
+    std::vector<char*> args;
+    args.push_back(argv[0]); // Program name
+    for (int i = 2; i < argc; ++i) {
+        args.push_back(argv[i]);
+    }
+
+    if (command == "postdom") {
+        cxxopts::Options options("cgquery postdom", "Postdominators analysis");
         options.add_options()
-            ("f, from", "Source function", cxxopts::value<std::string>())
-            ("t, to", "Target function", cxxopts::value<std::string>())
-            ("i, input", "Input file", cxxopts::value<std::string>())
-            ("h, help", "Print help");
+            ("n,node", "Postdominators of which node", cxxopts::value<std::string>())
+            ("e,exit", "Exit node", cxxopts::value<std::string>())
+            ("h,help", "Print help");
 
-
-
-        auto result = options.parse(argc, argv);
+        auto result = options.parse(static_cast<int>(args.size()), args.data());
 
         if (result.count("help")) {
             std::cout << options.help() << "\n";
             return 0;
         }
 
-        // TODO: Check if arguments/options are missing 
-
-        if (cg_name.empty()) {
-            std::cerr << "Input file required\n";
-            return 1;
-        }
-        if (!result.count("to")) {
-            std::cerr << "Target function required. Use -t or --to to specify target function" << std::endl;
-
+        std::string nodeName = result.count("node") ? result["node"].as<std::string>() : ""; 
+        std::string exitNode = result.count("exit") ? result["exit"].as<std::string>() : "";
+        auto* nodePtr = cg->getFirstNode(nodeName);
+        if (!nodePtr) {
+            std::cerr << "Node '" << nodeName << "' not found in CG.\n";
             return 1;
         }
 
-        std::string to = result["to"].as<std::string>();
-
-        // Read call-graph
-        auto fs = metacg::io::FileSource(cg_name);
-        std::unique_ptr<metacg::io::MCGReader> r1 = metacg::io::createReader(fs);
-        auto cg = r1->read();
-
-
-        metacg::analysis::ReachabilityAnalysis reachabilityAnalysis(cg.get());
-
-        if (!result.count("from")) {
-            return reachabilityAnalysis.existsPathBetween(cg->getMain(), cg->getFirstNode(to), true); 
+        auto* exitPtr = cg->getFirstNode(exitNode);
+        if (!exitPtr) {
+            std::cerr << "Node '" << exitNode << "' not found in CG.\n";
         }
-        else {
-            std::string from = result["from"].as<std::string>();
-            // NOTE: function names are not unique, should we use ids 
-            return reachabilityAnalysis.existsPathBetween(cg->getFirstNode(from), cg->getFirstNode(to), true);
+
+        auto postdom = metacg::analysis::computeDoms<
+            metacg::CgNode,
+            metacg::Callgraph,
+            metacg::analysis::TraverseDir::Backward>(*cg, *exitPtr);
+
+        const auto& data = postdom[nodePtr];
+        if (!data.initialized) {
+            std::cerr << "Postdominator analysis not initialized for node '" << nodeName << "'\n";
+            return 1;
+        }
+
+        std::cout << "Postdominators of " << nodeName << ":\n";
+        for (const auto* pdPtr : data.Doms) {
+            std::cout << "  " << pdPtr->getFunctionName() << "\n";
         }
     }
-    else if (command == "stat") {
-        cxxopts::Options options("cgquery stat", "return number of edges and nodes");
+    else if (command == "dom") {
+        cxxopts::Options options("cgquery doms", "Dominators analysis");
         options.add_options()
-            ("h, help", "Print help");
+            ("n,node", "Dominators of which node", cxxopts::value<std::string>())
+            ("e,entry", "Entry node", cxxopts::value<std::string>())
+            ("h,help", "Print help");
 
-
-        auto result = options.parse(args.size(), args.data());
+        auto result = options.parse(static_cast<int>(args.size()), args.data());
 
         if (result.count("help")) {
             std::cout << options.help() << "\n";
             return 0;
         }
 
-        auto fs = metacg::io::FileSource(cg_name);
-        std::unique_ptr<metacg::io::MCGReader> r1 = metacg::io::createReader(fs);
-        auto cg = r1->read();
+        std::string nodeName = result.count("node") ? result["node"].as<std::string>() : options.help();
+        std::string entry = result.count("node") ? result["node"].as<std::string>() : "main";
+        auto* nodePtr = cg->getFirstNode(nodeName);
+        if (!nodePtr) {
+            std::cerr << "Node '" << nodeName << "' not found in CG.\n";
+            return 1;
+        }
 
-        auto num_edges = cg->getEdges().size();
-        auto num_nodes = cg->getNodes().size();
+        std::string entryName = result.count("entry") ? result["entry"].as<std::string>() : "main";
+        auto* entryPtr = cg->getFirstNode(entryName);
+        if (!entryPtr) {
+            std::cerr << "Entry node '" << entryName << "' not found in CG.\n";
+            return 1;
+        }
 
-        std::cout << "--- Stats ---" << "\n"
-            << "# of nodes: " << num_nodes << "\n" 
-            << "# of edges: " << num_edges << "\n" 
-            << std::endl;
-        return 0;
+        auto dom = metacg::analysis::computeDoms<
+            metacg::CgNode,
+            metacg::Callgraph,
+            metacg::analysis::TraverseDir::Forward>(*cg, *entryPtr);
 
-    }
-    else {
-        std::cerr << "Unknown command: " << command << "\n";
-        return 1;
+        const auto& data = dom[nodePtr];
+        if (!data.initialized) {
+            std::cerr << "Dominator analysis not initialized for node '" << nodeName << "'\n";
+            return 1;
+        }
+
+        std::cout << "Dominators of " << nodeName << ":\n";
+        for (const auto* pdPtr : data.Doms) {
+            std::cout << "  " << pdPtr->getFunctionName() << "\n";
+        }
     }
 
     return 0;
 }
+
