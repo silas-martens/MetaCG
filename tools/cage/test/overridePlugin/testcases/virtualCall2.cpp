932fcf717829f50cc8433a1f15d7505e2b810281
Silas Martens <github@silasmartens.de>
2026-09-23 16:29:09 +0200

    fix tests

--- tools/cage/test/overridePlugin/testcases/virtualCall2.cpp
+++ tools/cage/test/overridePlugin/testcases/virtualCall2.cpp
@@ -21,15 +21,14 @@ int main() {
 
     return 1;
 }
-
 // CHECK-DAG: @[[A_ANNOT:[^ ]+]] = private unnamed_addr constant [12 x i8] c"_ZN1A3fooEv\00", section "llvm.metadata"
 // CHECK-DAG: @[[B_ANNOT:[^ ]+]] = private unnamed_addr constant [12 x i8] c"_ZN1B3fooEv\00", section "llvm.metadata"
 // CHECK-DAG: @[[PAYLOAD:[^ ]+]] = private unnamed_addr constant [15 x i8] c"overridePlugin\00"
-// CHECK-DAG: @[[ARGS_A:[^ ]+]] = private unnamed_addr constant { [15 x i8] } { [15 x i8] ptrtoint (ptr @[[PAYLOAD]] to [15 x i8]) }, section "llvm.metadata"
-// CHECK-DAG: @[[ARGS_B:[^ ]+]] = private unnamed_addr constant { [15 x i8] } { [15 x i8] ptrtoint (ptr @[[PAYLOAD]] to [15 x i8]) }, section "llvm.metadata"
+
+// CHECK-DAG: @[[ARGS:[^ ]+]] = private unnamed_addr constant { [15 x i8] } { [15 x i8] ptrtoint (ptr @[[PAYLOAD]] to [15 x i8]) }, section "llvm.metadata"
+// CHECK-DAG: @[[ARGS2:[^ ]+]] = private unnamed_addr constant { [15 x i8] } { [15 x i8] ptrtoint (ptr @[[PAYLOAD]] to [15 x i8]) }, section "llvm.metadata"
+
 // CHECK: @llvm.global.annotations = appending global
-// CHECK-SAME: { ptr @_ZN1B3fooEv, ptr @[[A_ANNOT]], ptr
-// CHECK-SAME: ptr @[[ARGS_A]] }
-// CHECK-SAME: { ptr @_ZN1C3fooEv, ptr @[[B_ANNOT]], ptr
-// CHECK-SAME: ptr @[[ARGS_B]] }
-// CHECK-NOT: { ptr @_ZN1C3fooEv, ptr @[[A_ANNOT]]
+// CHECK-DAG: ptr @_ZN1B3fooEv, ptr @[[A_ANNOT]]
+// CHECK-DAG: ptr @_ZN1C3fooEv, ptr @[[B_ANNOT]]
+// CHECK-NOT: ptr @_ZN1C3fooEv, ptr @[[A_ANNOT]]
--- tools/cage/test/overridePlugin/testcases/virtualDestructor.cpp
+++ tools/cage/test/overridePlugin/testcases/virtualDestructor.cpp
@@ -13,12 +13,10 @@ int main() {
     delete a;
     return 0;
 }
-
 // CHECK-DAG: @[[A_DTOR_ANNOT:[^ ]+]] = private unnamed_addr constant [10 x i8] c"_ZN1AD2Ev\00"
 // CHECK-DAG: @[[PAYLOAD:[^ ]+]] = private unnamed_addr constant [15 x i8] c"overridePlugin\00"
 // CHECK-DAG: @[[ARGS:[^ ]+]] = private unnamed_addr constant { [15 x i8] } { [15 x i8] ptrtoint (ptr @[[PAYLOAD]] to [15 x i8]) }, section "llvm.metadata"
+
 // CHECK: @llvm.global.annotations = appending global
-// CHECK-SAME: { ptr @_ZN1BD0Ev, ptr @[[A_DTOR_ANNOT]], ptr
-// CHECK-SAME: ptr @[[ARGS]] }
-// CHECK-SAME: { ptr @_ZN1BD2Ev, ptr @[[A_DTOR_ANNOT]], ptr
-// CHECK-SAME: ptr @[[ARGS]] }
+// CHECK-DAG: ptr @_ZN1BD0Ev, ptr @[[A_DTOR_ANNOT]],
+// CHECK-DAG: ptr @_ZN1BD2Ev, ptr @[[A_DTOR_ANNOT]],
